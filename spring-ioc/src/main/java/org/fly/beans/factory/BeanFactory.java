97117e509859ac10d4e3cbb321f2290ce6b25eeb
nooverfly <532967158@qq.com>
2017-08-21 11:11:33 +0800

    spring v1

---
+++ spring-ioc/src/main/java/org/fly/beans/factory/BeanFactory.java
@@ -0,0 +1,7 @@
+package org.fly.beans.factory;
+
+/**
+ * Created by admin on 2017/8/21.
+ */
+public interface BeanFactory {
+}
---
+++ src/main/java/org/fly/spring/SpringApplication.java
@@ -0,0 +1,7 @@
+package org.fly.spring;
+
+/**
+ * Created by admin on 2017/8/21.
+ */
+public class SpringApplication {
+}
